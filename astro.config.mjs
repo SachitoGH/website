42a3ae194e81d4287080faff07f085c361228dac
Sacha Senas <sachasenas@gmail.com>
2026-01-21 12:49:46 +0100

    small changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // This is required for the social media images and canonical URLs to work
+  site: 'https://surf-sidi-kaouki.netlify.app',
+
   vite: {
     plugins: [tailwindcss()]
   }
