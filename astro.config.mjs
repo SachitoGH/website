232974291814a07df667db7f6a6bc56fa7d35048
Sacha Senas <sachasenas@gmail.com>
2026-01-23 10:59:24 +0100

    new url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // This is required for the social media images and canonical URLs to work
-  site: 'https://surf-sidi-kaouki.netlify.app',
+  site: 'https://surf-sidi-kaouki.vercel.app',
 
   vite: {
     plugins: [tailwindcss()]
